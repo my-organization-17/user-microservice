68b8432d19353f0eeffd4c7f1bc58b5839ef6558
Dmytro Kotykhin <kotykhin_d@ukr.net>
2026-01-20 22:45:36 +0100

    add base tests

---
+++ src/health-check/tests/health-check.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { HealthCheckController } from '../health-check.controller';
+import { HealthCheckService } from '../health-check.service';
+
+describe('HealthCheckController', () => {
+  let controller: HealthCheckController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HealthCheckController],
+      providers: [HealthCheckService],
+    })
+      .overrideProvider(HealthCheckService)
+      .useValue({})
+      .compile();
+
+    controller = module.get<HealthCheckController>(HealthCheckController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
---
+++ src/token/tests/token.service.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { TokenService } from '../token.service';
+
+describe('TokenService', () => {
+  let service: TokenService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [TokenService],
+    })
+      .overrideProvider(TokenService)
+      .useValue({})
+      .compile();
+
+    service = module.get<TokenService>(TokenService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
